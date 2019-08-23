484befbb6c2048b672fcf7f355f3d3c694d65d4d
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-23 13:57:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def introduction(name)
-  print ""
+  puts `Hi, my name is #{name}.`
 end
 
 def introduction_with_language
