0f6239858233b073b688fb6ca10d7cc2e8d6a9c4
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-23 13:56:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,11 +3,13 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-def introduction
+def introduction(name)
+  print ""
+end
 
 def introduction_with_language
 
-
+end
 
 
 
